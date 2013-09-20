c70ccf072c71568ecf8ded9363283e025dc2ebb0
Brendan Obra <brendan_o'bra@cable.comcast.com>
2013-09-20 14:23:13 -0700

    more bootstrappy stuff

---
+++ automation/src/main/resources/puppet/modules/boosterlaunch/manifests/appserver.pp
@@ -0,0 +1,19 @@
+class boosterlaunch::appserver{
+	package{'openjdk-7-jdk':
+		ensure => present,
+	}
+	package{'jetty':
+		require => [Package['openjdk-7-jdk']]
+		ensure => 'present',
+	}
+	file{'/etc/default/jetty':
+		require => [Package['jetty']],
+		ensure => file,
+		content => hiera('jetty'),
+	}
+	service{'jetty':
+		require => [File['/etc/default/jetty']],
+		ensure => 'started',
+		enabled => 'true'
+	}
+}
