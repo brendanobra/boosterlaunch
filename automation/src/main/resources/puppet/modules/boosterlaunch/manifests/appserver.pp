086ff2a9f8f5c785c3af935d81eb96b29009950c
brendanobra <brendan@obrafamily.org>
2013-09-21 14:08:34 -0700

    changing

--- automation/src/main/resources/puppet/modules/boosterlaunch/manifests/appserver.pp
+++ automation/src/main/resources/puppet/modules/boosterlaunch/manifests/appserver.pp
@@ -2,18 +2,4 @@ class boosterlaunch::appserver{
 	package{'openjdk-7-jdk':
 		ensure => present,
 	}
-	package{'jetty':
-		require => [Package['openjdk-7-jdk']],
-		ensure => 'present',
-	}
-	file{'/etc/default/jetty':
-		require => [Package['jetty']],
-		ensure => file,
-		content => hiera('jetty'),
-	}
-	service{'jetty':
-		require => [File['/etc/default/jetty']],
-		ensure => 'running',
-		enable => 'true'
-	}
 }
--- automation/src/main/resources/scripts/gce/gce-up.sh
+++ automation/src/main/resources/scripts/gce/gce-up.sh
@@ -1,15 +1,5 @@
-wget -O /tmp/puppetlabs.repo http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
-dpkg -i /tmp/puppetlabs.repo
-apt-get update
-apt-get install --assume-yes git
-cd /tmp; git clone https://github.com/brendanobra/boosterlaunch.git 
-apt-get install --assume-yes puppet
-gem install hiera-file
-cp /tmp/boosterlaunch/automation/src/main/resources/etc/puppet/hiera.yaml /etc/puppet
-mkdir -p /etc/puppet/hieradata
-cp -R /tmp/boosterlaunch/hieradata/* /etc/puppet/hieradata
-mkdir -p /etc/facter/facts.d
-echo "role=appserver" >> /etc/facter/facts.d/role.txt
-echo "hiera_include(classes)" > /tmp/puppet.pp
-cp -R /tmp/boosterlaunch/automation/src/main/resources/puppet/modules/boosterlaunch /etc/puppet/modules
-puppet apply --debug /tmp/puppet.pp
+#!/bin/sh
+./up.sh
+sleep 60s
+./push.sh /home/brendan/projects/boosterlaunch/dist/boosterlaunch-1.0-SNAPSHOT.zip
+./do-provision.sh
